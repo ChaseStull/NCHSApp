848f2c24fe77dfdac5300a84baee70b247a461ae
Chase Stull <chaseastull@gmail.com>
2016-03-14 14:07:13 -0400

    Started Work on login Scripts

--- NCHSApp/www/login.php
+++ NCHSApp/www/login.php
@@ -2,8 +2,12 @@
 // define variables and set to empty values
 $Username = $Password = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $Username = test_input($_POST["Username"]);
-  $Password = test_input($_POST["Password"]);
-}
+  $Username = $_POST["Username"];
+  $Password = $_POST["Password"];
+
+  $file = fopen(sha1(sha1("Accounts"))/$Username.txt, r);
+
+  if ($Password == fread($file, 512)) {
+  	
+  }
 ?>
