5703ca5e9c79f2d82aac3000c4f459808a82df7a
Chase Stull <chaseastull@gmail.com>
2016-10-28 11:56:27 +0000

    Building Messenger Page 10/28/16

--- messenger.php
+++ messenger.php
@@ -1,12 +1,66 @@
 <?php
 	require("functions.php");
-	echo "
+	if(isset($_COOKIE["Student"]))
+	{
+		if($_GET["switch"]==true)
+		{
+			$threadLoc = $_GET["threadLoc"];
+			$thread = fopen($threadLoc, "r");
+			$messages = fread($thread, filesize($threadLoc));
+			$messagesArray = json_decode($messages, false, filesize($threadLoc));
+			fclose($thread);
+			
+			if($_COOKIE["Student"]==$messagesArray[0]||$_COOKIE["Student"]==$messagesArray[1])
+			{
+				$threads = displayThreads($_COOKIE["Student"], $_GET["type"]);
+			}
+		}
+		else
+		{
+			$threadList = fopen();
+		}
+		echo "
 	<html>
 		<head>
 			<title>Messaging</title>
+			<meta name='viewport' content='initial-scale=1, maximum-scale=1, user-scalable=no, width=device-width'>
+            <link rel='stylesheet' type='text/css' href='css/style.css'>
+            <meta charset='utf-8'>
 		</head>
 		<body>
+			<div style='width: 100%; background-color: white; overflow: hidden; height: 50px;'>
+                <div style='float: left;'>
+                    <b>Welcome, ".$_COOKIE["Student"]."</b>
+                </div>
+                <a href='logOut.php'>
+                    <div style='float: right; color: #bf1d32;'>
+                        <span><b>Sign Out</b></span>
+                    </div>
+                </a>
+                <div style='background-color: transparent; width: 4px; height: 17px; float: right;'></div>
+                <div style='background-color: black; width: 2px; height: 17px; float: right;'></div>
+                <div style='background-color: transparent; width: 4px; height: 17px; float: right;'></div>
+                <a href='studentHome.php'>
+		            <div style='float: right; color: #bf1d32;'>
+        	            <span>Home</span>
+                    </div>
+                </a>
+            </div>
+            <div style='width: 100%;'>
+                <a href='http://northcountyhs.org'>
+                    <img alt='This Image Cannot Be Loaded' src='img/nclogo.jpg' class='logo'>
+	            </a>
+			</div>
+			<div style='width: 100%; height: 100%; position: fixed; background-color: transparent;'>
+				<div style='width: 20%; height: 100%; background-color: white; float: left;>
+					<br>
+					<span>Conversations<span>
+					".$threads."
+				</div>
+			</div>
 		</body>
 	</html>
 	";
+	}
+	
 ?>
